7fb764a50fff170c25607084b4a26f875a8e9ff0
paugonpae67 <114425589+paugonpae67@users.noreply.github.com>
2024-04-12 08:41:00 +0200

    Initial commit

---
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantDetailScreen.js
@@ -0,0 +1,161 @@
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, View, FlatList, ImageBackground, Image, Pressable } from 'react-native'
+import { showMessage } from 'react-native-flash-message'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { getDetail } from '../../api/RestaurantEndpoints'
+import ImageCard from '../../components/ImageCard'
+import TextRegular from '../../components/TextRegular'
+import TextSemiBold from '../../components/TextSemibold'
+import * as GlobalStyles from '../../styles/GlobalStyles'
+import defaultProductImage from '../../../assets/product.jpeg'
+
+export default function RestaurantDetailScreen ({ navigation, route }) {
+  const [restaurant, setRestaurant] = useState({})
+
+  useEffect(() => {
+    async function fetchRestaurantDetail () {
+      try {
+        const fetchedRestaurant = await getDetail(route.params.id)
+        setRestaurant(fetchedRestaurant)
+      } catch (error) {
+        showMessage({
+          message: `There was an error while retrieving restaurant details (id ${route.params.id}). ${error}`,
+          type: 'error',
+          style: GlobalStyles.flashStyle,
+          titleStyle: GlobalStyles.flashTextStyle
+        })
+      }
+    }
+    fetchRestaurantDetail()
+  }, [])
+
+  const renderHeader = () => {
+    return (
+      <View>
+        <ImageBackground source={(restaurant?.heroImage) ? { uri: process.env.API_BASE_URL + '/' + restaurant.heroImage, cache: 'force-cache' } : undefined} style={styles.imageBackground}>
+          <View style={styles.restaurantHeaderContainer}>
+            <TextSemiBold textStyle={styles.textTitle}>{restaurant.name}</TextSemiBold>
+            <Image style={styles.image} source={restaurant.logo ? { uri: process.env.API_BASE_URL + '/' + restaurant.logo, cache: 'force-cache' } : undefined} />
+            <TextRegular textStyle={styles.description}>{restaurant.description}</TextRegular>
+            <TextRegular textStyle={styles.description}>{restaurant.restaurantCategory ? restaurant.restaurantCategory.name : ''}</TextRegular>
+          </View>
+        </ImageBackground>
+
+        <Pressable
+          onPress={() => navigation.navigate('CreateProductScreen', { id: restaurant.id })
+          }
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed
+                ? GlobalStyles.brandGreenTap
+                : GlobalStyles.brandGreen
+            },
+            styles.button
+          ]}>
+          <View style={[{ flex: 1, flexDirection: 'row', justifyContent: 'center' }]}>
+            <MaterialCommunityIcons name='plus-circle' color={'white'} size={20} />
+            <TextRegular textStyle={styles.text}>
+              Create product
+            </TextRegular>
+          </View>
+        </Pressable>
+      </View>
+    )
+  }
+
+  const renderProduct = ({ item }) => {
+    return (
+      <ImageCard
+        imageUri={item.image ? { uri: process.env.API_BASE_URL + '/' + item.image } : defaultProductImage}
+        title={item.name}
+      >
+        <TextRegular numberOfLines={2}>{item.description}</TextRegular>
+        <TextSemiBold textStyle={styles.price}>{item.price.toFixed(2)}€</TextSemiBold>
+        {!item.availability &&
+          <TextRegular textStyle={styles.availability }>Not available</TextRegular>
+        }
+      </ImageCard>
+    )
+  }
+
+  const renderEmptyProductsList = () => {
+    return (
+      <TextRegular textStyle={styles.emptyList}>
+        This restaurant has no products yet.
+      </TextRegular>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        ListHeaderComponent={renderHeader}
+        ListEmptyComponent={renderEmptyProductsList}
+        style={styles.container}
+        data={restaurant.products}
+        renderItem={renderProduct}
+        keyExtractor={item => item.id.toString()}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  row: {
+    padding: 15,
+    marginBottom: 5,
+    backgroundColor: GlobalStyles.brandSecondary
+  },
+  restaurantHeaderContainer: {
+    height: 250,
+    padding: 20,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  imageBackground: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center'
+  },
+  image: {
+    height: 100,
+    width: 100,
+    margin: 10
+  },
+  description: {
+    color: 'white'
+  },
+  textTitle: {
+    fontSize: 20,
+    color: 'white'
+  },
+  emptyList: {
+    textAlign: 'center',
+    padding: 50
+  },
+  button: {
+    borderRadius: 8,
+    height: 40,
+    marginTop: 12,
+    padding: 10,
+    alignSelf: 'center',
+    flexDirection: 'row',
+    width: '80%'
+  },
+  text: {
+    fontSize: 16,
+    color: 'white',
+    alignSelf: 'center',
+    marginLeft: 5
+  },
+  availability: {
+    textAlign: 'right',
+    marginRight: 5,
+    color: GlobalStyles.brandSecondary
+  }
+})
